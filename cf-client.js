574fc0c51f12bb1a5fdeeda0524a90633da536d8
Ahoorast <AhooraBozorgManesh@gmail.com>
2024-01-12 13:37:11 +0330

    complete client

--- cf-client.js
+++ cf-client.js
@@ -1,7 +1,3 @@
-import { which } from "bun";
-
-var server_url = 'http://127.0.0.1:3000';
-
 function getSubs() {
     return document.getElementsByClassName("status-frame-datatable")[0].getElementsByTagName('tr');
 }
@@ -15,46 +11,55 @@ function closeSub() {
 }
 
 function getSolution() {
-    return document.getElementsByClassName('source-popup')[1];
+    let solutionHTML = document.getElementsByClassName('source-popup')[1].getElementsByTagName('li');
+    let solutionText = "";
+    for (let i = 0; i < solutionHTML.length; i++) {
+        let lineText = "";
+        let lineHTML = solutionHTML[i].getElementsByTagName('span');
+        for (let j = 0; j < lineHTML.length; j++) {
+            lineText = lineText + lineHTML[j].innerText;
+        }
+        solutionText = solutionText + lineText + '\n';
+    }
+    return solutionText;
 }
 
 function getStatus(sub) {
     return sub.getElementsByClassName('submissionVerdictWrapper')[0].getAttribute('submissionverdict');
 }
 
-function makeRequest(url, method, data) {
-    $.ajax({
-        url: `{url}`,
-        type: method,
-        data: data,
-        success: function() { console.log("succ"); },
-        error: function() { console.log("error", data); },
-    });
+function getLink(sub) {
+    return sub.getElementsByTagName('td')[3].getElementsByTagName('a')[0].getAttribute('href');
 }
 
-function getAccepted() {
-    for (let i = 1; i < subs.length; i++) {
-        if (getStatus(subs[i]) !== 'OK') {
-            continue;
-        }
-        setTimeout(() => clickSub(subs[i]), 1000);
-        closeSub();
-    }
-}
-
-async function solutionToServer(solution) {
+async function solutionToServer(url, solution) {
     await fetch('http://127.0.0.1:3000/', {
         method: "POST",
         mode: "no-cors",
         headers: {
-            "Content-Type": "text/html",
+            "Content-Type": "text/plain",
         },
-        body: solution,
+        body: JSON.stringify({url: url, solution: solution}),
     });
 }
 
-async function handleSub(sub) {
-    clickSub(sub);
-    let data = getSolution().innerHTML;
-    await fetch(server_url);
+
+async function getAccepted() {
+    for (let i = 1; i < subs.length; i++) {
+        if (getStatus(subs[i]) !== 'OK') {
+            continue;
+        }
+        let url = getLink(subs[i]);
+        console.log(i);
+        clickSub(subs[i]);
+        await new Promise(resolve => setTimeout(resolve, 10000));
+        let solution = getSolution();
+        solutionToServer(solution, url);
+        console.log("HI", i);
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        closeSub();
+    }
 }
+
+subs = getSubs();
+getAccepted();
